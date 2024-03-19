271ffca950a9b6c243a817f0a5ede48b2b4dad28
Frankosviz <geom.francescopaolo@gmail.com>
2024-03-20 00:21:02 +0100

    changing method afterclass

--- js/script.js
+++ js/script.js
@@ -1,31 +1,37 @@
-// Creo la mia funzione senza contenuto
+// Inizio creando una funzione che assocerò al change del form
 
-function generateNewGridSquare(content){
-    
-    // Aggiungo un elemento con classe square che ho creato in css
-    
-        const newElement = document.createElement('article');
-        newElement.innerHTML = '<span>' + content + '</span>';
-        newElement.classList.add('square');
-        return newElement;
-    }
-    
-    // console.log(generateNewGridSquare);
-    
-    // Seleziono la griglia assegnandogli le classi css che ho creato
-    
-    const grid = document.getElementById('grid');
-    grid.classList.add('grid');
-    console.log(grid);
-    
-    // Con il ciclo for vado ad aggiungere tante volte fino a 100 i miei quadrati con i numeri all'interno
-    
-    for (let index = 0; index < 100; index++){
-        const square = generateNewGridSquare(index + 1);
-        // console.log(square);
-    
-        square.addEventListener('click', function(){
-        square.classList.add('square', 'bg-aqua');
-        console.log(index + 1);
-        });
-    }
+function setLevel(){
+    const level = levelEl.value;    
+    let cellsNumber;
+    // La variabile serve a determinare il valore di levelEl e associarlo al livello di difficoltà che sceglierà l\utente
+    switch (level) {
+        // usiamo lo switch
+        case "easy":
+        // default:
+          cellsNumber = 100;
+          break;
+        case "medium":
+            cellsNumber = 81;
+            break;
+        case "crazy":
+            cellsNumber = 49;
+            break;
+      }
+      return cellsNumber;{}
+}
+
+function drawSquare(div, content, bombs, maxscore){
+    // Creo newSquare grazie alla quale aggiungerò i quadrati
+    const newSquare = document.getElementById('div');
+    // Prendendo il div...
+    newSquare.classList.add('box');
+    // Assegnandogli classe box creata nel css...
+    newSquare.style.setProperty('--ms-box-dim', `calc(500px / ${dim} )`);
+    // La dimensione gia data in CSS...
+    newSquare.innerHTML = `<span class="invisible"> ${content} </span>`;
+    // Ed il contenuto inizialmente invisibile.
+}
+
+
+
+
